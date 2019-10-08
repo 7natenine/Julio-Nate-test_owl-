cd18df3093186d6f64e5acdaf8d51a7ba940a652
julio-crypt0 <hernandezjulio58@ymail.com>
2019-10-08 12:17:23 -0700

    #1 done

--- julio-nate-test_owl-/src/App.js
+++ julio-nate-test_owl-/src/App.js
@@ -1,26 +1,67 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+function App(props) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  props.store.participants.map((list) => {
+      if(list.inSession && list.onStage){
+      return ( 
+          <li className='List'>
+            <img src={list.avatar} height='80px' alt='avatar-img' />
+            <p>{list.name}</p>
+            <div class="info">On Stage<span class="dot"></span></div> 
+          </li>)
+    }
+    if(list.inSession){
+      return ( 
+          <li className='List'>
+            <img src={list.avatar} height='80px' alt='avatar-img' />
+            <p>{list.name}</p>
+            <div class="info"></div> 
+          </li>)
+    }
+    })
   );
 }
 
 export default App;
+ 
+
+
+
+// import React from 'react';
+// import List from './List'
+
+// import './App.css'
+
+// function App(props) {
+
+  
+//   return (
+//     <main className='App'>
+//       <header className="App-header">
+//         <h1>Trelloyes!</h1>
+//       </header>
+//       <div className="App-list">
+//     {props.store.lists.map(list=> {
+//       return (
+//           <List header={list.header} cards={list.cardIds.map(card =>{
+//             return(
+//               props.store.allCards[card]
+//             )
+//           })
+//           } key={list.id}/>   
+//         );
+//       })
+//     }      
+//       </div>
+//     </main>
+//   );
+// }
+
+// export default App;
+
+
+
+
+// ReactDOM.render(<App store={store}/>, document.getElementById('root'));
